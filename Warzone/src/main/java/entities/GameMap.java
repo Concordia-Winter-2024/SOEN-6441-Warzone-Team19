513ac87b712b126236f0d0241902f9f190382598
nenpatel2001 <158588935+nenpatel2001@users.noreply.github.com>
2024-02-15 12:12:28 -0500

    set getters validateMap

--- Warzone/src/main/java/entities/GameMap.java
+++ Warzone/src/main/java/entities/GameMap.java
@@ -1,7 +1,29 @@
 public class GameMap{
 
+    private HashMap<Integer, Continent> d_continents;
+	private HashMap<Integer, Country> d_countries;
+	private boolean d_isValid;
+
+    public GameMap() {
+		d_continents = new HashMap<>();
+		d_countries = new HashMap<>();
+		d_isValid = false;
+	}
+
     public String validateMap(){
         return "";
     }
+
+    public boolean getValidateStatus() {
+		return d_isValid;
+	}
+
+    public HashMap<Integer, Continent> getContinents() {
+		return d_continents;
+	}
+
+    public HashMap<Integer, Country> getCountries() {
+		return d_countries;
+	}
 
 }
