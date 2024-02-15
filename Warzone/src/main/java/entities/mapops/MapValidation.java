39c77b9a8fde077b63a4561b569f2495328265f4
nenpatel2001 <158588935+nenpatel2001@users.noreply.github.com>
2024-02-15 13:40:57 -0500

    implemented iterator method

--- Warzone/src/main/java/entities/mapops/MapValidation.java
+++ Warzone/src/main/java/entities/mapops/MapValidation.java
@@ -1,4 +1,42 @@
+import java.util.HashSet;
+import java.util.Set;
+
+import entities.Continent;
+import entities.Country;
+import entities.GameMap;
+
 public class MapValidation {
+
+    private boolean d_connectedGraph;
+	private boolean d_connectedSubGraph = true;
+	private GameMap d_gameMap;
+	private boolean d_emptyMap = false;
+	private boolean d_emptyContinent = false;
+	private boolean d_flag = false;
+	private boolean d_iteratingContinent = false;
+	private int d_currentContinentIteration = 0;
+
+    public MapValidation(GameMap p_gameMap) {
+		this.d_gameMap = p_gameMap;
+	}
+
+    public Set<Integer> countryIterator(Country p_currentCountry, Set<Integer> p_visitedCountryIds) {
+		if (p_visitedCountryIds.contains(p_currentCountry.getId())) {
+			return p_visitedCountryIds;
+		} else {
+			p_visitedCountryIds.add(p_currentCountry.getId());
+			for (Country l_nextCountry : p_currentCountry.getNeighborCountries()) {
+				if (d_iteratingContinent) {
+					if (l_nextCountry.getContinent().getId() == d_currentContinentIteration) {
+						p_visitedCountryIds = countryIterator(l_nextCountry, p_visitedCountryIds);
+					}
+				} else {
+					p_visitedCountryIds = countryIterator(l_nextCountry, p_visitedCountryIds);
+				}
+			}
+		}
+		return p_visitedCountryIds;
+	}
 
     public String validate(){
         return "";
