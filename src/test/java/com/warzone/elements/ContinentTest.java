333f044ae1f1b95c2d3f3342dee058cbfe38e105
Amal01111 <g_amal@live.concordia.ca>
2024-02-16 19:33:57 -0500

    added tests for continent

---
+++ src/test/java/com/warzone/elements/ContinentTest.java
@@ -0,0 +1,67 @@
+package com.warzone.elements;
+
+//package entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Tests for Continent
+ */
+public class ContinentTest {
+
+    /**
+     * This method is used to check if countries are removed
+     */
+    @Test
+    public void testCountriesRemoved()
+    {
+        Country l_Country = new Country(1, d_continent);
+        d_continent.addCountry(l_Country);
+        d_continent.removeCountry(l_Country);
+        assertEquals(0, d_continent.getCountriesSet().size());
+        assertFalse(d_continent.getCountriesSet().contains(l_Country));
+    }
+
+    Continent d_continent = new Continent(1, 3);
+
+    /**
+     * This is used to check if continent object is not null
+     */
+    @Test
+    public void testCheckNotNull()
+    {
+        assertNotNull(d_continent);
+    }
+
+    /**
+     * This method is used to check id of continent
+     */
+    @Test
+    public void testID()
+    {
+        assertEquals(1, d_continent.getId());
+    }
+
+    /**
+     * This is used to check control value of continent
+     */
+    @Test
+    public void testControlValue()
+    {
+        assertEquals(3, d_continent.getControlValue());
+    }
+
+    /**
+     * This is used to check if countries are added
+     */
+    @Test
+    public void testCountriesAdded()
+    {
+        Country l_Country = new Country(1, d_continent);
+        d_continent.addCountry(l_Country);
+        assertEquals(1, d_continent.getCountriesSet().size());
+        assertTrue(d_continent.getCountriesSet().contains(l_Country));
+    }
+}
