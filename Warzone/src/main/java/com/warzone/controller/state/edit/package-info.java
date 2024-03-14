6245e08f46a25937a76b2b4834303efa590edc66
Rushin-Makwana <ru_makwa@live.concordia.ca>
2024-03-14 17:05:54 -0400

    Added state pattern for edit map phase

---
+++ Warzone/src/main/java/com/warzone/controller/state/edit/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Edit phase which contains PreEdit and PostEdit class to load map in edit mode
+ * and to edit the loaded map.
+ */
+package com.warzone.controller.state.edit;
--- Warzone/src/main/java/com/warzone/elements/map/MapValidation.java
+++ Warzone/src/main/java/com/warzone/elements/map/MapValidation.java
@@ -1,4 +1,4 @@
-package com.warzone.elements.map;
+ epackage com.warzone.elements.map;
 
 import java.util.HashSet;
 import java.util.Set;
