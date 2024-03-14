ccea5c5063c6b9e2c045e8eb92a4c77f6935b88c
Rushin-Makwana <ru_makwa@live.concordia.ca>
2024-03-14 17:45:53 -0400

    Modified Observer class

--- Warzone/src/main/java/com/warzone/controller/GameInitialization.java
+++ Warzone/src/main/java/com/warzone/controller/GameInitialization.java
@@ -16,7 +16,7 @@ public class GameInitialization {
      */
     public String[] getCommand() {
         String l_userCommand;
-        System.out.print("\033[1;34m"+" * "+"\033[0m");
+        System.out.print("\033[1;34m" + " * " + "\033[0m");
         l_userCommand = l_scanner.nextLine();
         return l_userCommand.split(" ");
     }
@@ -30,7 +30,7 @@ public class GameInitialization {
         Commands l_commands = new Commands();
         GameInitialization l_gameInitialization = new GameInitialization();
 
-        System.out.println("\033[1;93m"+"=====> Welcome to Warzone <====="+"\033[0m");
+        System.out.println("\033[1;93m" + "=====> Welcome to Warzone <=====" + "\033[0m");
         while (true) {
             String[] l_splittedCommandString = l_gameInitialization.getCommand();
             if (l_splittedCommandString[0].equals("exit")) {
@@ -38,42 +38,63 @@ public class GameInitialization {
             }
             System.out.println(l_commands.executeCommand(l_splittedCommandString));
         }
-        System.out.print("\033[1;93m"+"\n😃 Thank you for playing 😃 "+"\033[0m");
+        System.out.print("\033[1;93m" + "\n😃 Thank you for playing 😃 " + "\033[0m");
         l_gameInitialization.l_scanner.close();
     }
-}
-
-public class Observable {
-    private List<Observer> d_observers = new ArrayList<Observer>();
 
     /**
-     * Method to attach a view to the model.
-     *
-     * @param p_observer: view to be added to the list of observers that are to be notified.
+     * Interface for the Observer, which specifies the implementation of the update function in all views.
      */
-    public void attach(Observer p_observer) {
-        this.d_observers.add(p_observer);
+
+    public interface Observer {
+
+        /**
+         * method to be implemented that responds to  notification generally by
+         * querying the model object and presenting its most recent state
+         *
+         * @param p_observableState: The object that the subject passes through (observable)
+         *                         Though it's not always the case,
+         *                         this object is frequently the subject.
+         */
+        public void update(Observable p_observableState);
     }
 
-    /**
-     * Method to detach a view from the model.
-     *
-     * @param p_observer: view to be removed from the list of observers.
-     */
-    public void detach(Observer p_observer) {
-        if (!d_observers.isEmpty()) {
-            d_observers.remove(p_observer);
+
+    public class Observable {
+        private List<Observer> d_observers = new ArrayList<Observer>();
+
+        /**
+         * Method to attach a view to the model.
+         *
+         * @param p_observer: view that has to be added to the list of observers that are to be notified.
+         */
+        public void attach(Observer p_observer) {
+            this.d_observers.add(p_observer);
+        }
+
+        /**
+         * Method to detach a view from the model.
+         *
+         * @param p_observer: view that has o be removed from the list of observers.
+         */
+        public void detach(Observer p_observer) {
+            if (!d_observers.isEmpty()) {
+                d_observers.remove(p_observer);
+            }
+        }
+
+        /**
+         * It notifies all the views attached to the model.
+         *
+         * @param p_observable: object that contains the information to be observed.
+         */
+        public void notifyObservers(Observable p_observable) {
+            for (Observer l_observer : d_observers) {
+                l_observer.update(p_observable);
+            }
         }
     }
 
-    /**
-     * Method to notify all the views attached to the model.
-     *
-     * @param p_observable: object that contains the information to be observed.
-     */
-    public void notifyObservers(Observable p_observable) {
-        for (Observer l_observer : d_observers) {
-            l_observer.update(p_observable);
-        }
-    }
 }
+
+
