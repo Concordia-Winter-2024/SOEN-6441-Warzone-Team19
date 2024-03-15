2a78769824040a5990fd39e8483673c8fb033770
TusharJain-cloud <tu_jai@live.concordia.ca>
2024-03-15 18:52:14 -0400

    Update duplicate continent

--- Warzone/src/test/java/com/warzone/elements/PlayerTest.java
+++ Warzone/src/test/java/com/warzone/elements/PlayerTest.java
@@ -119,12 +119,15 @@ public class PlayerTest {
         assertFalse(d_player.getCountries().keySet().contains(10));
     }
 
+    /**
+     * Test to check if the duplicate continent exist
+     */
     @Test
     public void testAddDuplicateContinent() {
         Continent l_continent = new Continent(1, 3);
         d_player.addContinent(l_continent);
         d_player.addContinent(l_continent);
-        assertEquals(1, d_player.getContinents().size()-1);
+        assertEquals(1, d_player.getContinents().size());
     }
 
 }
